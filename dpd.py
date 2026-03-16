e139bf35290a6484a828fa3a50685ee6bbe8b08b
Patrick B Warren <patrickbwarren@gmail.com>
2026-03-16 20:53:28 +0000

    g(r) added ; numerous fixes

--- dpd.py
+++ dpd.py
@@ -22,10 +22,12 @@
 # Can be run with map/reduce as for example,
 # ./mapper.py dpd.py --header=puretest--seed=12345 --ntrial=10 --nthrow=10^5 --njobs=20 --run
 
-import sys
 import argparse
 import numpy as np
 from itertools import product
+
+def kM_replace(s):
+    return s.replace('k', '*1e3').replace('M', '*1e6')
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('--header', required=True, help='set the name of the output files')
@@ -37,8 +39,9 @@ parser.add_argument('-r', '--rho', default=3.0, type=float, help='density, defau
 parser.add_argument('--npart', default='rho*vol', help='number of particles, default computed')
 parser.add_argument('--nmove', default='npart', help='number of MC moves per sweep, default npart')
 parser.add_argument('--nequil', default=10, type=int, help='number of equilibration sweeps, default 10')
-parser.add_argument('--nsamp', default=0, type=int, help='number of additional samples, default 0')
 parser.add_argument('--delta', default=0.2, type=float, help='trial displacement, default 0.2')
+parser.add_argument('--rmax', default=4.0, type=float, help='max radius for rdf, default 4.0')
+parser.add_argument('--nbins', default=80, type=int, help='number of bins in rdf, default 80')
 parser.add_argument('-A', '--A', default=25.0, type=float, help='repulsion amplitude, default 25.0')
 parser.add_argument('-v', '--verbose', action='count', default=0, help='increasing verbosity')
 args = parser.parse_args()
@@ -50,20 +53,15 @@ ep_file = '%s__%d_ep.dat' % (args.header, pid)
 gr_file = '%s__%d_gr.dat' % (args.header, pid)
 log_file = '%s.log' % args.header
 
-A = args.A
-rho = args.rho
-delta = args.delta
-
-def kM_replace(s):
-    return s.replace('k', '*1e3').replace('M', '*1e6')
-
+A, rho, delta = args.A, args.rho, args.delta
 es, esby2, vol = args.es, args.es/2, args.es**3
+
 npart = eval('int(%s)' % kM_replace(args.npart))
 rho = args.rho if args.npart is None else npart/vol
 nmove = eval('int(%s)' % kM_replace(args.nmove))
-nequil, nsamp = args.nequil, args.nsamp
+nequil = args.nequil
 
-pos = rng.uniform(0.0, es, size=(npart, 3)) # initialise particle positions
+pos = rng.uniform(0, es, size=(npart, 3)) # initialise particle positions
 
 ncell = int(es) # number of cells along one axis
 cell_size = es / ncell # cell size
@@ -87,9 +85,9 @@ def brute_force():
         rsq = np.sum(Δr**2)
         if rsq < 1:
             r = np.sqrt(rsq)
-            energy = energy + (A/2)*(1-r)**2
-            virial = virial + A*r*(1-r)
-    return energy/vol, rho + virial/(3*vol)
+            energy += (A/2)*(1-r)**2
+            virial += A*r*(1-r)
+    return 3*rho/2 + energy/vol, rho + virial/(3*vol)
 
 def energy_pressure():
     energy, virial = 0, 0
@@ -105,9 +103,9 @@ def energy_pressure():
                     rsq = np.sum(Δr**2)
                     if rsq < 1:
                         r = np.sqrt(rsq)
-                        energy = energy + (A/2)*(1-r)**2
-                        virial = virial + A*r*(1-r)
-    return energy/vol, rho + virial/(3*vol)
+                        energy += (A/2)*(1-r)**2
+                        virial += A*r*(1-r)
+    return 3*rho/2 + energy/vol, rho + virial/(3*vol)
 
 def part_energy(i, cell, pos_i):
     energy = 0
@@ -120,7 +118,7 @@ def part_energy(i, cell, pos_i):
                 rsq = np.sum(Δr**2)
                 if rsq < 1:
                     r = np.sqrt(rsq)
-                    energy = energy + (A/2)*(1-r)**2
+                    energy += (A/2)*(1-r)**2
     return energy
 
 def test_energy():
@@ -128,73 +126,79 @@ def test_energy():
     print(f'cell list methods  =\t{e}\t{p}')
     e, p = brute_force()
     print(f'brute force method =\t{e}\t{p}')
-    energy = 0
+    energy = 3*npart/2
     for i in range(npart):
         cell = (pos[i]/cell_size).astype(int) # calculate cell coordinates
-        energy = energy + 0.5*part_energy(i, cell, pos[i])
+        energy += 0.5*part_energy(i, cell, pos[i])
     print(f'part_energy        =\t{energy/vol}')
 
 if args.verbose > 1:
     test_energy()
 
-def mc_moves(nmove):
+# This is the actual Monte-Carlo algorithm
+
+for sweep in range(nequil): # do a number of sweeps of nmove trial moves
     naccept = 0
     parts = rng.integers(0, npart, size=nmove)
     disps = rng.normal(0.0, delta, size=(nmove, 3))
     probs = rng.random(size=nmove)
-    for i, d, p in zip(parts, disps, probs):
+    for i, d, prob in zip(parts, disps, probs):
         old_pos = pos[i]
         old_cell = (old_pos/cell_size).astype(int)
         old_energy = part_energy(i, old_cell, old_pos)
         new_pos = (old_pos + d) % es
         new_cell = (new_pos/cell_size).astype(int)
         new_energy = part_energy(i, new_cell, new_pos)
-        if (p < np.exp(-(new_energy-old_energy))):
-            naccept = naccept + 1
+        if (prob < np.exp(-(new_energy-old_energy))): # acceptance criterion
+            naccept += 1
             pos[i] = new_pos
             contents[tuple(old_cell)].remove(i)
             contents[tuple(new_cell)].add(i)
-    return naccept/nmove
-
-for sweep in range(nequil):
-    acc_rat = mc_moves(nmove)
-    e, p = energy_pressure()
+    (e, p), a = energy_pressure(), naccept/nmove
     if args.verbose:
-        print('equilibration:\t%i\t%g\t%g\t%g' % (sweep, acc_rat, e, p))
-
-results = [(e, p, acc_rat)]
-
-for samp in range(nsamp):
-    acc_rat = mc_moves(nmove)
-    e, p = energy_pressure()
-    results.append((e, p, acc_rat))
-    if args.verbose:
-        print('production:\t%i\t%g\t%g\t%g' % (samp, acc_rat, e, p))
-
-#    mode = 'w' if samp == 0 else 'a'
-#    with open(gr_file, mode) as f:
-#        for ig in range(nbins):
-#            r = (ig + 0.5) / nbins
-#            area_annulus = np.pi * ((ig+1)**2 - ig**2) / nbins**2
-#            g = 4.0 * gr_bins[ig] / (nthrows * area_annulus)
-#            f.write('%g\tgr__%g\n' % (g, r))
-
-with open(ep_file, 'w') as f:
-    for e, p, acc_rat in results:
-        f.write('%g\te\n' % e)
-        f.write('%g\tp\n' % p)
-        f.write('%g\tacc_rat\n' % acc_rat)
+        print('equilibration:\t%i\t%g\t%g\t%g' % (sweep, e, p, a))
 
 if args.verbose > 1:
     test_energy()
 
-run_opts = [sys.argv[0], f'--header={args.header}', f'--seed={args.seed}',
-            f'--nequil={nequil}', f'--nsamp={nsamp}', f'--nmove={nmove}',
+# record final energy, pressure, and acceptance ratio
+
+with open(ep_file, 'w') as f:
+    f.write('%g\te\n' % e)
+    f.write('%g\tp\n' % p)
+    f.write('%g\ta\n' % a)
+
+# measure and record pair distribution function at this point
+
+nbins, rmax = args.nbins, args.rmax
+gr_bins = np.zeros(1+nbins, dtype=int) # integer here since counting 'hits'
+Δg = rmax / nbins
+
+for i, j in pairs:
+    Δr = pos[j] - pos[i]
+    Δr = Δr - np.where(Δr > esby2, es, 0) + np.where(Δr < -esby2, es, 0)
+    r = np.sqrt(np.sum(Δr**2))
+    ig = min(nbins, int(r/Δg)) # catch all pairs
+    gr_bins[ig] += 1 # final bin is a dustbin for pairs not within range
+
+norm = np.sum(gr_bins) # include dustbin at the end, should be |pairs| × # samples
+ig = np.arange(nbins)
+r = (ig + 0.5) * Δg # midpoint
+vshell = 4*np.pi/3 * (3*ig**2 + 3*ig + 1) * Δg**3 # volume of shell around midpoint
+g = gr_bins[:-1] * vol / (norm * vshell) # exclude 'dustbin' at end
+
+with open(gr_file, 'w') as f:
+    for i in range(nbins):
+        f.write('%g\tgr__%g\n' % (g[i], r[i]))
+
+# make final reports
+
+run_opts = [f'--header={args.header}', f'--seed={args.seed}',
+            f'--nequil={nequil}', f'--nmove={nmove}',
             f'--A={A}', f'--rho={rho}', f'--es={es}']
 
-reports = ['command_line: ' + ' '.join(sys.argv),
-           'full_command: ' + ' '.join(run_opts),
-           'data collected for: ep']
+reports = ['opts: ' + ' '.join(run_opts),
+           'data collected for: ep, gr']
 
 if args.verbose:
     for line in reports:
