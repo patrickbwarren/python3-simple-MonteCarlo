8e671ca5fe5cd43389906fad81055220c0ad8502
Patrick B Warren <patrickbwarren@gmail.com>
2026-03-29 11:39:13 +0100

    implement walls

---
+++ dpd_nvt.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Run a DPD Monte-Carlo simulation
+# This file is part of the simple Monte-Carlo collection.
+
+# This is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# Copyright (c) 2026 Patrick B Warren <patrick.warren@stfc.ac.uk>
+
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+# Can be run with map/reduce as for example,
+# ./mapper.py dpd.py --header=test --seed=12345 --es=6 --njobs=20 --run
+
+import argparse
+import numpy as np
+from itertools import product
+
+def kM_replace(s):
+    return s.replace('k', '*1e3').replace('M', '*1e6')
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--header', default=None, help='set the name of the output files')
+parser.add_argument('--seed', default=12345, type=int, help='the RNG seed, default 12345')
+parser.add_argument('--process', default=0, type=int, help='process number, default 0')
+parser.add_argument('--njobs', default=1, type=int, help='the number of condor jobs, deault 1')
+parser.add_argument('-e', '--es', default=10.0, type=float, help='box size, default 10.0')
+parser.add_argument('-r', '--rho', default=3.0, type=float, help='density, default 3.0')
+parser.add_argument('-A', '--A', default=25.0, type=float, help='repulsion amplitude, default 25.0')
+parser.add_argument('--npart', default='rho*vol', help='number of particles, default computed')
+parser.add_argument('--nmove', default='npart', help='number of MC moves per sweep, default npart')
+parser.add_argument('--nequil', default=10, type=int, help='number of equilibration sweeps, default 10')
+parser.add_argument('--delta', default=0.2, type=float, help='trial displacement, default 0.2')
+parser.add_argument('--rmax', default=4.0, type=float, help='max radius for rdf, default 4.0')
+parser.add_argument('--nbins', default=80, type=int, help='number of bins in rdf, default 80')
+parser.add_argument('-v', '--verbose', action='count', default=0, help='increasing verbosity')
+args = parser.parse_args()
+
+pid, njobs = args.process, args.njobs
+rng = np.random.default_rng(seed=args.seed).spawn(njobs)[pid] # select a local RNG stream
+
+A, rho, ΔR = args.A, args.rho, args.delta
+es, esby2, vol = args.es, args.es/2, args.es**3
+
+npart = eval('int({})'.format(kM_replace(args.npart)))
+rho = args.rho if args.npart is None else npart/vol
+nmove = eval('int({})'.format(kM_replace(args.nmove)))
+nequil = args.nequil
+
+pos = rng.uniform(0, es, size=(npart, 3)) # initialise particle positions
+wld = np.zeros(npart) # will contain the weighted local densities
+
+ncell = int(es) # number of cells along one axis
+cell_size = es / ncell # cell size
+cell_coord = list(range(ncell)) # list of coords along one axis
+all_cell_coords = product(cell_coord, cell_coord, cell_coord) # coords of all cells, as tuples
+contents = dict([(cell, set()) for cell in all_cell_coords]) # empty cell sets, indexed by cell coords
+
+for i in range(npart): # go through all particles
+    cell = (pos[i]/cell_size).astype(int) # calculate cell coordinates
+    contents[tuple(cell)].add(i) # add particle to set of particles in cell
+
+neighbours = [np.array(x, dtype=int) for x in product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1])]
+
+pairs = [(i, j) for i, j in product(range(npart), range(npart)) if i < j]
+
+def brute_force():
+    energy, virial, wgt = 0, 0, 0
+    for i, j in pairs:
+        Δr = pos[j] - pos[i]
+        Δr = Δr - np.where(Δr > esby2, es, 0) + np.where(Δr < -esby2, es, 0)
+        rsq = np.sum(Δr**2)
+        if rsq < 1:
+            r = np.sqrt(rsq)
+            energy += (A/2)*(1-r)**2
+            virial += A*r*(1-r)
+            wgt += 1/4*(1-r)**2 # note normalisation
+    return 3*rho/2 + energy/vol, rho + virial/(3*vol), 2*wgt/npart
+
+def energy_pressure_mean_wld():
+    wld[:] = 0 # has side effect of building the wld array
+    energy, virial = 0, 0
+    for i in range(npart):
+        pos_i = pos[i]
+        cell = (pos_i/cell_size).astype(int)
+        for neighbour in neighbours:
+            neighbour_cell = (cell + neighbour) % ncell
+            for j in contents[tuple(neighbour_cell)]:
+                if i < j:
+                    Δr = pos[j] - pos_i
+                    Δr = Δr - np.where(Δr > esby2, es, 0) + np.where(Δr < -esby2, es, 0)
+                    rsq = np.sum(Δr**2)
+                    if rsq < 1:
+                        r = np.sqrt(rsq)
+                        energy += (A/2)*(1-r)**2
+                        virial += A*r*(1-r)
+                        wgt = 1/4*(1-r)**2 # note normalisation
+                        wld[i] += wgt
+                        wld[j] += wgt
+    return 3*rho/2 + energy/vol, rho + virial/(3*vol), np.mean(wld)
+
+def part_energy(i, cell, pos_i):
+    energy = 0
+    for neighbour in neighbours:
+        neighbour_cell = (cell + neighbour) % ncell
+        for j in contents[tuple(neighbour_cell)]:
+            if i != j:
+                Δr = pos[j] - pos_i
+                Δr = Δr - np.where(Δr > esby2, es, 0) + np.where(Δr < -esby2, es, 0)
+                rsq = np.sum(Δr**2)
+                if rsq < 1:
+                    r = np.sqrt(rsq)
+                    energy += (A/2)*(1-r)**2
+    return energy
+
+def tot_part_energy(): # test of above
+    energy = 3*npart/2
+    for i in range(npart):
+        cell = (pos[i]/cell_size).astype(int) # calculate cell coordinates
+        energy += 0.5*part_energy(i, cell, pos[i])
+    return energy/vol
+
+def test_energy():
+    print('cell list methods  =\t{}\t{}\t{}'.format(*energy_pressure_mean_wld()))
+    print('brute force method =\t{}\t{}\t{}'.format(*brute_force()))
+    print('tot part energy    =\t{}'.format(tot_part_energy()))
+
+if args.verbose > 1:
+    test_energy()
+
+# This is the actual Monte-Carlo algorithm
+
+for sweep in range(nequil): # do a number of sweeps of nmove trial moves
+    naccept = 0
+    parts = rng.integers(0, npart, size=nmove)
+    disps = rng.normal(0.0, ΔR, size=(nmove, 3))
+    probs = rng.random(size=nmove)
+    for i, disp, prob in zip(parts, disps, probs):
+        old_pos = pos[i]
+        old_cell = (old_pos/cell_size).astype(int)
+        old_energy = part_energy(i, old_cell, old_pos)
+        new_pos = (old_pos + disp) % es
+        new_cell = (new_pos/cell_size).astype(int)
+        new_energy = part_energy(i, new_cell, new_pos)
+        if (prob < np.exp(-(new_energy-old_energy))): # acceptance criterion
+            naccept += 1
+            pos[i] = new_pos
+            contents[tuple(old_cell)].remove(i)
+            contents[tuple(new_cell)].add(i)
+    (e, p, w), a = energy_pressure_mean_wld(), naccept/nmove
+    if args.verbose:
+        print('equilibration: {:3d} {:0.5f} {:0.5f}'.format(sweep, e, a))
+
+final_stats = [e, p, w, a]
+
+if args.verbose > 1:
+    test_energy()
+
+# Widom insertion for chemical potential
+
+def even_parity(cell): # used to implement checkboard pattern
+    return (cell[0] + cell[1] + cell[2]) % 2 == 0
+
+origin_pos = rng.uniform(0, 1, size=3) # random insertion point in first cell
+insert_eng = np.array([part_energy(npart, cell, origin_pos+np.array(cell)) for cell in contents if even_parity(cell)])
+mu = -np.log(vol*np.mean(np.exp(-insert_eng))/(npart+1)) # insert_eng collects the insertion energies
+final_stats.append(mu)
+
+# pair distribution functions (totals)
+
+nbins, Δg = args.nbins, args.rmax / args.nbins
+
+count = np.zeros(nbins, dtype=int) # integer here since counting 'hits'
+sumnr = np.zeros(nbins) # for the wld, presumed already computed
+
+i = np.arange(nbins)
+rmid = (i + 0.5) * Δg # midpoint
+vshell = 4*np.pi/3 * (3*i**2 + 3*i + 1) * Δg**3 # volume of shell around midpoint
+
+for i, j in pairs:
+    Δr = pos[j] - pos[i]
+    Δr = Δr - np.where(Δr > esby2, es, 0) + np.where(Δr < -esby2, es, 0)
+    r = np.sqrt(np.sum(Δr**2))
+    k = int(r/Δg)
+    if k < nbins:
+        count[k] += 1
+        sumnr[k] += 0.5*(wld[i] + wld[j])
+
+gr = count * vol / (len(pairs) * vshell)
+
+run_opts = [f'--header={args.header}', f'--seed={args.seed}',
+            f'--nequil={nequil}', f'--nmove={nmove}',
+            f'--A={A}', f'--npart={npart}', f'--es={es}']
+
+if args.verbose > 1:
+    print('opts:', ' '.join(run_opts))
+
+if args.header is not None:
+
+    dd, ff, ss = '{:d}', '{:0.8f}', '{:s}'
+
+    stats_file = f'{args.header}__{pid:d}_stats.dat'
+    fmt_string = '\t'.join([ff, ss]) + '\n'
+    with open(stats_file, 'w') as f:
+        for i, code in enumerate('epwam'):
+            f.write(fmt_string.format(final_stats[i], code))
+
+    rdfs_file = f'{args.header}__{pid:d}_rdfs.dat'
+    fmt_string = '\t'.join([dd, dd, ff, dd, ff, ff]) + '\n'
+    with open(rdfs_file, 'w') as f:
+        for i in range(nbins):
+            f.write(fmt_string.format(pid, i, rmid[i], count[i], sumnr[i], gr[i]))
+
+    files = [stats_file, rdfs_file]
+    concats = ['rdfs']
+
+    if args.process == 0:
+        log_file = f'{args.header}.log'
+        with open(log_file, 'w') as f:
+            f.write('# opts: ' + ' '.join(run_opts) + '\n')
+            f.write('# reduce data for: stats\n')
+            f.write('# concatenate data for: ' + ', '.join(concats) + '\n')
+
+if args.verbose:
+    print('data >', ', '.join(files))
+
+# end of code
---
+++ dpd_walls.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Run a DPD Monte-Carlo simulation
+# This file is part of the simple Monte-Carlo collection.
+
+# This is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# Copyright (c) 2026 Patrick B Warren <patrick.warren@stfc.ac.uk>
+
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+# Can be run with map/reduce as for example,
+# ./mapper.py dpd.py --header=test --seed=12345 --es=6 --njobs=20 --run
+
+import argparse
+import numpy as np
+from itertools import product
+
+def kM_replace(s):
+    return s.replace('k', '*1e3').replace('M', '*1e6')
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--header', default=None, help='set the name of the output files')
+parser.add_argument('--seed', default=12345, type=int, help='the RNG seed, default 12345')
+parser.add_argument('--process', default=0, type=int, help='process number, default 0')
+parser.add_argument('--njobs', default=1, type=int, help='the number of condor jobs, deault 1')
+parser.add_argument('-e', '--es', default=10.0, type=float, help='box size, default 10.0')
+parser.add_argument('-r', '--rho', default=3.0, type=float, help='density, default 3.0')
+parser.add_argument('-A', '--A', default=25.0, type=float, help='repulsion amplitude, default 25.0')
+parser.add_argument('--npart', default='rho*vol', help='number of particles, default computed')
+parser.add_argument('--nmove', default='npart', help='number of MC moves per sweep, default npart')
+parser.add_argument('--nequil', default=10, type=int, help='number of equilibration sweeps, default 10')
+parser.add_argument('--delta', default=0.2, type=float, help='trial displacement, default 0.2')
+parser.add_argument('--nbins', default=40, type=int, help='number of bins in density profile, default 40')
+parser.add_argument('-v', '--verbose', action='count', default=0, help='increasing verbosity')
+args = parser.parse_args()
+
+pid, njobs = args.process, args.njobs
+rng = np.random.default_rng(seed=args.seed).spawn(njobs)[pid] # select a local RNG stream
+
+A, rho, ΔR = args.A, args.rho, args.delta
+es, esby2, vol = args.es, args.es/2, args.es**3
+zlo, zhi = 0.5, es-0.5
+
+npart = eval('int({})'.format(kM_replace(args.npart)))
+rho = args.rho if args.npart is None else npart/vol
+nmove = eval('int({})'.format(kM_replace(args.nmove)))
+nequil = args.nequil
+
+pos = rng.uniform(0, es, size=(npart, 3)) # initialise particle positions
+z = pos[:, 2]
+pos = pos[(z > zlo) & (z < zhi), :] # keep only particles between walls
+npart = pos.shape[0] # recount
+vol = es**2*(zhi-zlo) # recalculate
+rho = npart / vol # ditto
+
+ncell = int(es) # number of cells along one axis
+cell_size = es / ncell # cell size
+cell_coord = list(range(ncell)) # list of coords along one axis
+all_cell_coords = product(cell_coord, cell_coord, cell_coord) # coords of all cells, as tuples
+contents = dict([(cell, set()) for cell in all_cell_coords]) # empty cell sets, indexed by cell coords
+
+for i in range(npart): # go through all particles
+    cell = (pos[i]/cell_size).astype(int) # calculate cell coordinates
+    contents[tuple(cell)].add(i) # add particle to set of particles in cell
+
+neighbours = [np.array(x, dtype=int) for x in product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1])]
+
+pairs = [(i, j) for i, j in product(range(npart), range(npart)) if i < j]
+
+def brute_force():
+    energy, virial = 0, 0
+    for i, j in pairs:
+        Δr = pos[j] - pos[i]
+        Δr = Δr - np.where(Δr > esby2, es, 0) + np.where(Δr < -esby2, es, 0)
+        rsq = np.sum(Δr**2)
+        if rsq < 1:
+            r = np.sqrt(rsq)
+            energy += (A/2)*(1-r)**2
+            virial += A*r*(1-r)
+    return 3*rho/2 + energy/vol, rho + virial/(3*vol)
+
+def energy_pressure():
+    energy = 0
+    virial = np.zeros(3)
+    for i in range(npart):
+        pos_i = pos[i]
+        cell = (pos_i/cell_size).astype(int)
+        for neighbour in neighbours:
+            neighbour_cell = (cell + neighbour) % ncell
+            for j in contents[tuple(neighbour_cell)]:
+                if i < j:
+                    Δr = pos[j] - pos_i
+                    Δr = Δr - np.where(Δr > esby2, es, 0) + np.where(Δr < -esby2, es, 0)
+                    rsq = np.sum(Δr**2)
+                    if rsq < 1:
+                        r = np.sqrt(rsq)
+                        energy += (A/2)*(1-r)**2
+                        virial += Δr**2 / r * A*(1-r) # resolves into components
+    return 3*rho/2 + energy/vol, rho + virial/vol
+
+def part_energy(i, cell, pos_i):
+    energy = 0
+    for neighbour in neighbours:
+        neighbour_cell = (cell + neighbour) % ncell
+        for j in contents[tuple(neighbour_cell)]:
+            if i != j:
+                Δr = pos[j] - pos_i
+                Δr = Δr - np.where(Δr > esby2, es, 0) + np.where(Δr < -esby2, es, 0)
+                rsq = np.sum(Δr**2)
+                if rsq < 1:
+                    r = np.sqrt(rsq)
+                    energy += (A/2)*(1-r)**2
+    return energy
+
+def tot_part_energy(): # test of above
+    energy = 3*npart/2
+    for i in range(npart):
+        cell = (pos[i]/cell_size).astype(int) # calculate cell coordinates
+        energy += 0.5*part_energy(i, cell, pos[i])
+    return energy/vol
+
+def test_energy():
+    e, p = energy_pressure()
+    print('cell list methods  =\t{}\t{}'.format(e, np.mean(p)))
+    print('brute force method =\t{}\t{}'.format(*brute_force()))
+    print('tot part energy    =\t{}'.format(tot_part_energy()))
+
+if args.verbose > 1:
+    test_energy()
+
+# This is the actual Monte-Carlo algorithm
+
+for sweep in range(nequil): # do a number of sweeps of nmove trial moves
+    naccept = 0
+    parts = rng.integers(0, npart, size=nmove)
+    disps = rng.normal(0.0, ΔR, size=(nmove, 3))
+    probs = rng.random(size=nmove)
+    for i, disp, prob in zip(parts, disps, probs):
+        old_pos = pos[i]
+        old_cell = (old_pos/cell_size).astype(int)
+        old_energy = part_energy(i, old_cell, old_pos)
+        new_pos = (old_pos + disp) % es
+        if new_pos[2] < zlo or new_pos[2] > zhi: # reject if falls outside walls
+            continue
+        new_cell = (new_pos/cell_size).astype(int)
+        new_energy = part_energy(i, new_cell, new_pos)
+        if (prob < np.exp(-(new_energy-old_energy))): # acceptance criterion
+            naccept += 1
+            pos[i] = new_pos
+            contents[tuple(old_cell)].remove(i)
+            contents[tuple(new_cell)].add(i)
+    (e, p), a = energy_pressure(), naccept/nmove
+    if args.verbose:
+        print('equilibration: {:3d} {:0.5f} {:0.5f}'.format(sweep, e, a))
+
+final_stats = [e, p, a]
+
+if args.verbose > 1:
+    test_energy()
+
+# density profile in z direction
+
+nbins = args.nbins
+counts, edges = np.histogram(pos[:, 2], nbins, range=(0, es), density=False)
+midpoint = 0.5*(edges[1:]+edges[:-1])
+density = counts * nbins / (es**3) 
+
+run_opts = [f'--header={args.header}', f'--seed={args.seed}',
+            f'--nequil={nequil}', f'--nmove={nmove}',
+            f'--A={A}', f'--npart={npart}', f'--es={es}']
+
+if args.verbose > 1:
+    print('opts:', ' '.join(run_opts))
+
+if args.header is not None:
+
+    dd, ff, ss = '{:d}', '{:0.8f}', '{:s}'
+
+    stats_file = f'{args.header}__{pid:d}_stats.dat'
+    fmt_string = '\t'.join([ff, ss]) + '\n'
+    with open(stats_file, 'w') as f:
+        for i, code in enumerate('epa'):
+            if code == 'p':
+                for j, dirn in enumerate('xyz'):
+                    f.write(fmt_string.format(final_stats[i][j], f'{code}{dirn}{dirn}'))
+            else:
+                f.write(fmt_string.format(final_stats[i], code))
+
+    zprof_file = f'{args.header}__{pid:d}_zprof.dat'
+    fmt_string = '\t'.join([ff, ff]) + '\n'
+    with open(zprof_file, 'w') as f:
+        for i in range(nbins):
+            f.write(fmt_string.format(density[i], midpoint[i]))
+
+    files = [stats_file, zprof_file]
+
+    if args.process == 0:
+        log_file = f'{args.header}.log'
+        with open(log_file, 'w') as f:
+            f.write('# opts: ' + ' '.join(run_opts) + '\n')
+            f.write('# reduce data for: stats, zprof\n')
+
+if args.verbose:
+    print('data >', ', '.join(files))
+
+# end of code
